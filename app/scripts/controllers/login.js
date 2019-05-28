52c95e4be0b2ee239062abcf13a728a878f79532
Marcos Nils <marcosnils@gmail.com>
2019-05-28 16:57:21 -0300

    Don't redirect to form

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -10,10 +10,10 @@
 angular.module('yapp')
   .controller('LoginCtrl', function($scope, $location, pwdService, $stateParams, $window) {
 
-    if (!$stateParams.aliId) {
-      $window.location.href = 'https://dockr.ly/ee-workshop-test';
-      return;
-    }
+    //if (!$stateParams.aliId) {
+      //$window.location.href = 'https://dockr.ly/ee-workshop-test';
+      //return;
+    //}
 
 
     pwdService.assignSession()
